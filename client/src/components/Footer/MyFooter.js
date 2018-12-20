cb3bb0f0955325dccb2d190847de0c5c12562be2
Patricia LG <patri.lopg5@gmail.com>
2018-12-20 11:17:36 +0100

    color icons footer done

--- client/src/components/Footer/MyFooter.js
+++ client/src/components/Footer/MyFooter.js
@@ -1,24 +1,28 @@
 import React, { Component } from "react";
 import "./MyFooter.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default class MyFooter extends Component {
   render() {
+
     return (
       <div>
         <div className="iconsFooter">
-          <Link to="/my-kitchen">
-            <i className="fas fa-utensils fas-footer" style={{ fontSize: 32,color: "#877C73"}}/>
-          </Link>
-          <Link to="/recipes">
-          <i className="fas fa-book fas-footer" style={{ fontSize: 32,color: "#877C73"}}></i>
-          </Link>
-          <Link to="/my-shopping-list">
-          <i className="fas fa-shopping-cart fas-footer"  style={{ fontSize: 32, color: "#877C73"}}></i>
-          </Link>
-          <Link to="/my-profile">
-          <i className="fas fa-user fas-footer" style={{ fontSize: 32,color: "#877C73"}}></i>
-          </Link>
+          <NavLink activeClassName="whiteIcon" to="/my-kitchen">
+            <i className="fas fa-utensils fas-footer iconStyle changeColor" />
+          </NavLink>
+
+          <NavLink activeClassName="whiteIcon" to="/recipes">
+            <i className="fas fa-book fas-footer iconStyle changeColor" />
+          </NavLink>
+
+          <NavLink activeClassName="whiteIcon" to="/my-shopping-list">
+            <i className="fas fa-shopping-cart fas-footer iconStyle changeColor" />
+          </NavLink>
+
+          <NavLink activeClassName="whiteIcon" to="/my-profile">
+            <i className="fas fa-user fas-footer iconStyle changeColor" />
+          </NavLink>
         </div>
       </div>
     );
